7007f6a9a0602a50072ed6c74edb3717240f66d3
Carrington <hartis.c99@gmail.com>
2024-11-20 13:17:21 -0500

    Moved fields and methods to the PolicyHolder class

--- src/Policy.java
+++ src/Policy.java
@@ -2,35 +2,17 @@ public class Policy {
     // Attributes
     private int policyNumber;
     private String providerName;
-    private String policyHolderFirstName;
-    private String policyHolderLastName;
-    private int policyHolderAge;
-    private String policyHolderSmokingStatus;
-    private double policyHolderHeight;
-    private double policyHolderWeight;
 
    // No arg constructor, default's for all fields
     public Policy() {
         policyNumber = 1;
         providerName = "None";
-        policyHolderFirstName = "Jane";
-        policyHolderLastName = "Doe";
-        policyHolderAge = 25;
-        policyHolderSmokingStatus = "non-smoker";
-        policyHolderHeight = 5.3;
-        policyHolderWeight = 110;
     }
 
     // Constructor that accepts arguments
-    public Policy(int number, String provider, String firstName, String lastName, int age, String smokingStatus, double height, double weight) {
+    public Policy(int number, String provider) {
         policyNumber = number;
         providerName = provider;
-        policyHolderFirstName = firstName;
-        policyHolderLastName = lastName;
-        policyHolderAge = age;
-        policyHolderSmokingStatus = smokingStatus;
-        policyHolderHeight = height;
-        policyHolderWeight = weight;
     }
 
     // Setter methods
@@ -44,36 +26,6 @@ public class Policy {
         providerName = provider;
     }
 
-    // @param firstName - the policyholder's first name
-    public void setPolicyHolderFirstName(String firstName) {
-        policyHolderFirstName = firstName;
-    }
-
-    // @param lastName - the policyholder's last name
-    public void setPolicyHolderLastName(String lastName) {
-        policyHolderLastName = lastName;
-    }
-
-    // @param age - the policyholder's age
-    public void setPolicyHolderAge(int age) {
-        policyHolderAge = age;
-    }
-
-    // @param smokingStatus - the policyholder's smoking status
-    public void setPolicyHolderSmokingStatus(String smokingStatus) {
-        policyHolderSmokingStatus = smokingStatus;
-    }
-
-    // @param height - the policyholder's height
-    public void setPolicyHolderHeight(double height) {
-        policyHolderHeight = height;
-    }
-
-    // @param weight - the policyholder's weight
-    public void setPolicyHolderWeight(double weight) {
-        policyHolderWeight = weight;
-    }
-
     // Getter Methods
     // @return policyNumber - the policy number
     public int getPolicyNumber() {
@@ -83,36 +35,6 @@ public class Policy {
     // @return providerName - the provider's name
     public String getProviderName() {
         return providerName;
-    }
-
-    // @return policyHolderFirstName - the policyholder's first name
-    public String getPolicyHolderFirstName() {
-        return policyHolderFirstName;
-    }
-
-    // @return policyHolderLastName - the policyholder's last name
-    public String getPolicyHolderLastName() {
-        return policyHolderLastName;
-    }
-
-    // @return policyHolderAge - the policyholder's age
-    public int getPolicyHolderAge() {
-        return policyHolderAge;
-    }
-
-    // @return policyHolderSmokingStatus - the policyholder's smoking status
-    public String getPolicyHolderSmokingStatus() {
-        return policyHolderSmokingStatus;
-    }
-
-    // @return policyHolderHeight - the policyholder's height
-    public double getPolicyHolderHeight() {
-        return policyHolderHeight;
-    }
-
-    // @return policyHolderWeight - the policyholder's weight
-    public double getPolicyHolderWeight() {
-        return policyHolderWeight;
     }
 
     // Method for calculating policyholder's BMI
--- src/PolicyHolder.java
+++ src/PolicyHolder.java
@@ -1,2 +1,88 @@
-package PACKAGE_NAME;public class PolicyHolder {
+public class PolicyHolder {
+    private String policyHolderFirstName;
+    private String policyHolderLastName;
+    private int policyHolderAge;
+    private String policyHolderSmokingStatus;
+    private double policyHolderHeight;
+    private double policyHolderWeight;
+
+    // No arg constructor
+    public PolicyHolder() {
+        policyHolderFirstName = "Jane";
+        policyHolderLastName = "Doe";
+        policyHolderAge = 0;
+        policyHolderSmokingStatus = "non-smoker";
+        policyHolderHeight = 0;
+        policyHolderWeight = 0;
+    }
+
+    // Constructor that accepts arguments
+    public PolicyHolder(String firstName, String lastName, int age, String smokingStatus, double height, double weight) {
+        policyHolderFirstName = firstName;
+        policyHolderLastName = lastName;
+        policyHolderAge = age;
+        policyHolderSmokingStatus = smokingStatus;
+        policyHolderHeight = height;
+        policyHolderWeight = weight;
+    }
+
+    // @param firstName - the policyholder's first name
+    public void setPolicyHolderFirstName(String firstName) {
+        policyHolderFirstName = firstName;
+    }
+
+    // @param lastName - the policyholder's last name
+    public void setPolicyHolderLastName(String lastName) {
+        policyHolderLastName = lastName;
+    }
+
+    // @param age - the policyholder's age
+    public void setPolicyHolderAge(int age) {
+        policyHolderAge = age;
+    }
+
+    // @param smokingStatus - the policyholder's smoking status
+    public void setPolicyHolderSmokingStatus(String smokingStatus) {
+        policyHolderSmokingStatus = smokingStatus;
+    }
+
+    // @param height - the policyholder's height
+    public void setPolicyHolderHeight(double height) {
+        policyHolderHeight = height;
+    }
+
+    // @param weight - the policyholder's weight
+    public void setPolicyHolderWeight(double weight) {
+        policyHolderWeight = weight;
+    }
+
+    // @return policyHolderFirstName - the policyholder's first name
+    public String getPolicyHolderFirstName() {
+        return policyHolderFirstName;
+    }
+
+    // @return policyHolderLastName - the policyholder's last name
+    public String getPolicyHolderLastName() {
+        return policyHolderLastName;
+    }
+
+    // @return policyHolderAge - the policyholder's age
+    public int getPolicyHolderAge() {
+        return policyHolderAge;
+    }
+
+    // @return policyHolderSmokingStatus - the policyholder's smoking status
+    public String getPolicyHolderSmokingStatus() {
+        return policyHolderSmokingStatus;
+    }
+
+    // @return policyHolderHeight - the policyholder's height
+    public double getPolicyHolderHeight() {
+        return policyHolderHeight;
+    }
+
+    // @return policyHolderWeight - the policyholder's weight
+    public double getPolicyHolderWeight() {
+        return policyHolderWeight;
+    }
 }
